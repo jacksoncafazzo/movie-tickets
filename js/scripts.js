d2344bdfde22476aabb2c564efd06bf2602b3e83
jackson cafazzo and jordan meier <doctorjuno@gmail.com>
2016-01-26 15:53:50 -0800

    Add quantity property to Ticket constructor and adjusted price method and specs accordingly

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
-function Ticket (name, time, age) {
+function Ticket (name, time, age, quantity) {
   this.name = name;
   this.time = time;
   this.age = age;
+  this.quantity = quantity;
 }
 
 Ticket.prototype.price = function () {
+
   if (this.time <= "16:00"){
-    return "$5";
+    var price = 5;
   } else if (this.time > "16:00" && this.age < 60) {
-    return "$10";
+    var price = 10;
   } else if (this.time > "16:00" && this.age >= 60) {
-    return "$6";
+    var price = 6;
   } else {
     alert("Please fill out all fields");
   }
+  // var totalPrice = this.quantity * price
+  return "$" + (this.quantity * price);
 }
 
 $(document).ready(function() {
@@ -22,14 +26,18 @@ $(document).ready(function() {
     var chosenMovie = $("select#movieName").val();
     var chosenMovieTime = $("input#movieTime").val();
     var chosenAge = $("input#userAge").val();
-    var newTicket = new Ticket(chosenMovie, chosenMovieTime, chosenAge);
+    var chosenQuantity = $("input#ticketQuantity").val();
+    var newTicket = new Ticket(chosenMovie, chosenMovieTime, chosenAge, chosenQuantity);
     $(".movie-name").text(chosenMovie);
     $(".movie-time").text(chosenMovieTime);
     $(".movie-price").text(newTicket.price());
 
+
     $("select#movieName").val("");
     $("input#movieTime").val("");
     $("input#userAge").val("");
+    $("input#ticketQuantity").val("");
+
 
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,23 +1,24 @@
 describe('Ticket', function () {
   it("creates a new ticket with the given properties", function () {
-    var testTicket = new Ticket("Space Damon", "15:30", 16);
+    var testTicket = new Ticket("Space Damon", "15:30", 16, 3);
     expect(testTicket.name).to.equal("Space Damon");
     expect(testTicket.time).to.equal("15:30");
     expect(testTicket.age).to.equal(16);
+    expect(testTicket.quantity).to.equal(3);
   });
 
   it("creates a discounted price for matinee", function () {
-    var testTicket = new Ticket("Space Damon", "15:30", 16)
+    var testTicket = new Ticket("Space Damon", "15:30", 16, 1)
     expect(testTicket.price()).to.equal("$5");
   });
 
   it("creates a regular price for evening shows", function () {
-    var testTicket = new Ticket("Space Damon", "16:30", 16)
+    var testTicket = new Ticket("Space Damon", "16:30", 16, 1)
     expect(testTicket.price()).to.equal("$10");
   });
 
   it("creates a discount price based on age if not a matinee", function () {
-    var testTicket = new Ticket("Space Damon", "16:30", 65)
+    var testTicket = new Ticket("Space Damon", "16:30", 65, 1)
     expect(testTicket.price()).to.equal("$6");
   });
 
