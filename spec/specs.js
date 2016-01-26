d7a5fbde4fecc199e112a76ae3bc6446093118dd
jackson cafazzo and jordan meier <jbirdmeier@gmail.com>
2016-01-26 15:04:01 -0800

    Change times to military in specs

--- spec/specs.js
+++ spec/specs.js
@@ -1,23 +1,23 @@
 describe('Ticket', function () {
   it("creates a new ticket with the given properties", function () {
-    var testTicket = new Ticket("Space Damon", "3:30", 16);
+    var testTicket = new Ticket("Space Damon", "15:30", 16);
     expect(testTicket.name).to.equal("Space Damon");
-    expect(testTicket.time).to.equal("3:30");
+    expect(testTicket.time).to.equal("15:30");
     expect(testTicket.age).to.equal(16);
   });
 
   it("creates a discounted price for matinee", function () {
-    var testTicket = new Ticket("Space Damon", "3:30", 16)
+    var testTicket = new Ticket("Space Damon", "15:30", 16)
     expect(testTicket.price()).to.equal("$5");
   });
 
   it("creates a regular price for evening shows", function () {
-    var testTicket = new Ticket("Space Damon", "4:30", 16)
+    var testTicket = new Ticket("Space Damon", "16:30", 16)
     expect(testTicket.price()).to.equal("$10");
   });
 
   it("creates a discount price based on age if not a matinee", function () {
-    var testTicket = new Ticket("Space Damon", "4:30", 65)
+    var testTicket = new Ticket("Space Damon", "16:30", 65)
     expect(testTicket.price()).to.equal("$6");
   });
 
