6b707b36c2eb6d1429cf7ba1ab1e24f93e942dbf
jackson cafazzo and jordan meier <jbirdmeier@gmail.com>
2016-01-26 15:03:27 -0800

    Add user interface logic to scripts.js in the submit event listener

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,31 @@ function Ticket (name, time, age) {
 }
 
 Ticket.prototype.price = function () {
-  if (this.time <= "4:00"){
+  if (this.time <= "16:00"){
     return "$5";
-  } else if (this.time > "4:00" && this.age < 60) {
+  } else if (this.time > "16:00" && this.age < 60) {
     return "$10";
-  } else if (this.time > "4:00" && this.age >= 60) {
+  } else if (this.time > "16:00" && this.age >= 60) {
     return "$6";
   } else {
     alert("Please fill out all fields");
   }
 }
+
+$(document).ready(function() {
+  $("form#movies").submit(function (event) {
+    event.preventDefault();
+    var chosenMovie = $("select#movieName").val();
+    var chosenMovieTime = $("input#movieTime").val();
+    var chosenAge = $("input#userAge").val();
+    var newTicket = new Ticket(chosenMovie, chosenMovieTime, chosenAge);
+    $(".movie-name").text(chosenMovie);
+    $(".movie-time").text(chosenMovieTime);
+    $(".movie-price").text(newTicket.price());
+
+    $("select#movieName").val("");
+    $("input#movieTime").val("");
+    $("input#userAge").val("");
+
+  });
+});
